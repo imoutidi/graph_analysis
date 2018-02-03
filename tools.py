4ad4bfb43c83dcda351ddff52b76677cbb4c2132
imoutidi <imoutidi@gmail.com>
2018-02-03 17:43:04 +0000

    Computed ALL entities metrics from 13/1 until 2/2 in parallel.

--- tools.py
+++ tools.py
@@ -5,6 +5,7 @@ from datetime import date, timedelta
 from igraph import *
 import pymongo
 import graph_metrics
+import multiprocessing
 
 
 def form_graph(c_date, r_type, e_type):
@@ -85,49 +86,45 @@ def count_articles(last_date):
     return art_num
 
 
-def calculate_metrics_lists(end_date):
+def calculate_metrics_lists(end_date, relation_type, entity_type):
     # The date of the creation of the first graph
     s_date = date(2018, 1, 13)
 
-    entities_types = ["P"]
-    relations_types = ["Sentence"]
     date_range_list = list(date_range(s_date, end_date + timedelta(days=1)))
+    # each element of the outer list corresponds to a date
+    date_dir = "/home/iraklis/PycharmProjects/graph_analysis/node_metrics/"
+    metrics_lists_dict = dict()
+    for current_date in date_range_list:
+        current_graph = form_graph(current_date, relation_type, entity_type)
 
-    # Article Article_Sentence Sentence
-    for relation_type in relations_types:
-        # P L O LO PL PO PLO
-        for entity_type in entities_types:
-            # each element of the outer list corresponds to a date
-            date_dir = "/home/iraklis/PycharmProjects/graph_analysis/node_metrics/"
-            metrics_lists_dict = dict()
-            for current_date in date_range_list:
-                current_graph = form_graph(current_date, relation_type, entity_type)
+        metrics_lists_dict['degree_list'] = current_graph.degree()
+        metrics_lists_dict['weighted_degree_list'] = graph_metrics.weighted_degree(current_graph)
+        metrics_lists_dict['betweenness_list'] = current_graph.betweenness(directed=False)
+        metrics_lists_dict['closeness_list'] = current_graph.closeness()
+        metrics_lists_dict['eigenvector_list'] = current_graph.eigenvector_centrality(directed=False)
+        metrics_lists_dict['pagerank_list'] = current_graph.personalized_pagerank(directed=False)
+        metrics_lists_dict['Names'] = current_graph.vs['name']
 
-                metrics_lists_dict['degree_list'] = current_graph.degree()
-                metrics_lists_dict['weighted_degree_list'] = graph_metrics.weighted_degree(current_graph)
-                metrics_lists_dict['betweenness_list'] = current_graph.betweenness(directed=False)
-                metrics_lists_dict['closeness_list'] = current_graph.closeness()
-                metrics_lists_dict['eigenvector_list'] = current_graph.eigenvector_centrality(directed=False)
-                metrics_lists_dict['pagerank_list'] = current_graph.personalized_pagerank(directed=False)
-                # metrics_lists_dict_list['Names'].append()
+        year_path = date_dir + relation_type + "/" + str(current_date.year)
+        if not os.path.exists(year_path):
+            os.makedirs(year_path)
+        month_path = year_path + "/" + str(current_date.month)
+        if not os.path.exists(month_path):
+            os.makedirs(month_path)
+        week_path = month_path + "/" + str(current_date.isocalendar()[1])
+        if not os.path.exists(week_path):
+            os.makedirs(week_path)
+        day_path = week_path + "/" + str(current_date.day)
+        if not os.path.exists(day_path):
+            os.makedirs(day_path)
 
-                year_path = date_dir + relation_type + "/" + str(current_date.year)
-                if not os.path.exists(year_path):
-                    os.makedirs(year_path)
-                month_path = year_path + "/" + str(current_date.month)
-                if not os.path.exists(month_path):
-                    os.makedirs(month_path)
-                week_path = month_path + "/" + str(current_date.isocalendar()[1])
-                if not os.path.exists(week_path):
-                    os.makedirs(week_path)
-                day_path = week_path + "/" + str(current_date.day)
-                if not os.path.exists(day_path):
-                    os.makedirs(day_path)
-
-                # save_metrics = open("/home/iraklis/PycharmProjects/graph_analysis/pickle_files/"
-                #                     + relation_type + "_" + entity_type + "_dict.pickle", "wb")
-                # pickle.dump(metrics_lists_dict_list, save_metrics)
-                # save_metrics.close()
+        save_metrics = open("/home/iraklis/PycharmProjects/graph_analysis/node_metrics/"
+                            + relation_type + "/" + str(current_date.year) + "/"
+                            + str(current_date.month) + "/" + str(current_date.isocalendar()[1]) + "/"
+                            + str(current_date.day) + "/" + entity_type + "_dict.pickle", "wb")
+        pickle.dump(metrics_lists_dict, save_metrics)
+        save_metrics.close()
+    print("end off " + relation_type + " " + entity_type)
 
 
 # Here we will calculate the common nodes similarities for the graph
@@ -167,6 +164,22 @@ def affinity_matrix(end_date):
                 sorted_bet_list = sorted(range(len(bet_list)), key=lambda k: bet_list[k], reverse=True)
 
 
+def node_metrics_parallel(fns, in_date):
+    proc = []
+    # The computations on the PLO graphs are taking most of the time so we
+    # assign one core for each PLO graph (second for loop)
+    ent_types = ["P", "L", "O", "LO", "PL", "PO", "PLO"]
+    rel_types = ["Article", "Sentence", "Article_Sentence"]
+    for ent_type in ent_types:
+        for rel_type in rel_types:
+            p = multiprocessing.Process(target=fns, args=(in_date, rel_type, ent_type,))
+            p.start()
+            proc.append(p)
+    for pr in proc:
+        pr.join()
+
+
 if __name__ == "__main__":
-    n_date = date(2018, 2, 1)
-    calculate_metrics_lists(n_date)
+    n_date = date(2018, 2, 2)
+    # calculate_metrics_lists(n_date)
+    node_metrics_parallel(calculate_metrics_lists, n_date)
